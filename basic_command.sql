52f304ae6d78afa38035635585a938ddcd2054b1
snehadomadiya <snehadomadiya1617@gmail.com>
2024-01-28 15:04:30 +0100

    create table

---
+++ basic_command.sql
@@ -0,0 +1,6 @@
+show DATABASES;
+
+SHOW TABLES;
+
+create database student;
+use student;
---
+++ createTable.sql
@@ -0,0 +1,7 @@
+create table student_info(
+	Id int NOT NULL AUTO_INCREMENT,
+    firstName varchar(255) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
+    mobile int NOT NULL,
+    primary key(Id)
+);
